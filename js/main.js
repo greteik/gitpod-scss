5d3314ee3f584df9fbbc5964f648fa138ac915bd
Dmitry <165547891+greteik@users.noreply.github.com>
2026-02-11 16:09:46 +0300

    upd 4
    добавил модальное окно, сделал новую секцию, добавил файлы

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-console.log("привет мир");
+const LoginBtn = document.querySelector('.header_button');
+
+LoginBtn.addEventListener('click', ()=> {
+    LoginBtn.style.backgroundColor = 'orange';
+})
+
+const modal = document.getElementById("modalLogin");
+const openBtn = document.querySelector('.header_button');
+const closeBtn = document.getElementById("closeModal");
+
+// modal
+openBtn.addEventListener('click', () => {
+    modal.classList.add('active'); 
+});
+
+closeBtn.addEventListener('click', () => {
+    modal.classList.remove('active');
+});
+
+modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.classList.remove('active');
+    }
+});
+
+const modalBtnReg = document.querySelector('.modal-btn');
+const loginText = document.querySelector('.logintext');
+const formModal = document.querySelectorAll('.formModal');
+
+modalBtnReg.addEventListener('click', ()=> {
+    loginText.textContent = 'успешный вход!';
+})
+
